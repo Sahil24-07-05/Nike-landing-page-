3b1ea15aa4211be549e82839c48b12fcd2de5c86
Sahil24-07-05 <sabarwalsahil24@gmail.com>
2025-07-18 16:05:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./Hero.css";
+import Navigation from "./navigation";
+import Hero from "./Hero";
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
